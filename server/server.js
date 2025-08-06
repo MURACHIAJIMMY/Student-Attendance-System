2ab82c280d9a4b1312a93010999b7dfd9f8bb1ea
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-08-06 18:15:25 +0300

    gitignore, backend set up and configuration

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Student Attendance API running...')
+})
+
+const PORT = process.env.PORT || 5000
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}).catch(err => console.error(err))
