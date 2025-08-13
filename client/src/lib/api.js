ea7cea0e719fb9adb39e09fd5a96a1300006ca53
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-08-13 17:13:34 +0300

    updated api for cookies

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,9 +1,13 @@
-// src/lib/api.js
 import axios from "axios";
+
+const token = localStorage.getItem("token");
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : undefined,
+  },
 });
 
 export default api;
