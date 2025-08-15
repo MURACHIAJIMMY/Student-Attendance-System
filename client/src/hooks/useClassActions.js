6150036d0da78c0eea2a661ed3c505312c055661
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-08-15 16:15:55 +0300

    updated export here

--- client/src/hooks/useClassActions.js
+++ client/src/hooks/useClassActions.js
@@ -1,7 +1,9 @@
+// src/hooks/useClassContext.js
+
 import { useContext } from "react";
 import { ClassContext } from "@/context/ClassContext";
 
-export function useClassContext() {
+export default function useClassContext() {
   const context = useContext(ClassContext);
   if (!context) throw new Error("useClassContext must be used within ClassProvider");
   return context;
