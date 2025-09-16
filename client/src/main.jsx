1281ddc2533fee1b6adf3f6173df9bf3b8be4678
MURACHIA JIMMY <jamesmurachia84@gmail.com>
2025-09-16 14:02:54 +0300

    main update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
+
 // import React from "react";
 // import ReactDOM from "react-dom/client";
 // import App from "./App.jsx";
 // import "./index.css";
 // import { AuthProvider } from "@/context/AuthContext";
-// import { TeacherProvider } from "@/context/TeacherContext"; // ✅ Add this
+// import { TeacherProvider } from "@/context/TeacherProvider"; // ✅ updated path
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
@@ -20,14 +21,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "@/context/AuthContext";
-import { TeacherProvider } from "@/context/TeacherProvider"; // ✅ updated path
+import { ClassProvider } from "@/context/ClassContext"; // ✅ add this
+import { TeacherProvider } from "@/context/TeacherProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <TeacherProvider>
-        <App />
-      </TeacherProvider>
+      <ClassProvider> {/* ✅ wrap TeacherProvider */}
+        <TeacherProvider>
+          <App />
+        </TeacherProvider>
+      </ClassProvider>
     </AuthProvider>
   </React.StrictMode>
 );
